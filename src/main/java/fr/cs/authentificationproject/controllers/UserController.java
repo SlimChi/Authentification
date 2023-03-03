3bb3c3ea1ed193ef8e2f03565778cabeda88e7eb
Slimane <slimane.chihati@gmail.com>
2023-03-03 03:09:12 +0100

    first commit

---
+++ src/main/java/fr/cs/authentificationproject/controllers/UserController.java
@@ -0,0 +1,61 @@
+package fr.cs.authentificationproject.controllers;
+
+import fr.cs.authentificationproject.dto.UserDto;
+import fr.cs.authentificationproject.entities.User;
+import fr.cs.authentificationproject.services.UserService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.websocket.server.PathParam;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+@Tag(name = "User")
+public class UserController {
+
+
+    private final UserService userService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<UserDto>> findAll() {
+        return ResponseEntity.ok(userService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<User>> getUserById(@PathParam("id") Integer id){
+
+        Optional<User> user = userService.getUserById(id);
+        if(user.isPresent())
+            return ResponseEntity.ok(user);
+        else
+            return ResponseEntity.notFound().build();
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity updateUser(@PathParam("id")Integer id,
+                                     @RequestParam String firstName,
+                                     @RequestParam String lastName,
+                                     @RequestParam String email,
+                                     @RequestParam String password){
+
+        userService.updateUser(id,firstName,lastName,email,password);
+
+        return ResponseEntity.ok().build();
+
+    }
+
+    @DeleteMapping("/{user-id}")
+    public ResponseEntity<Void> delete(
+            @PathVariable("user-id") Integer userId
+    ) {
+        userService.delete(userId);
+        return ResponseEntity.accepted().build();
+    }
+
+}
---
+++ src/main/java/fr/cs/authentificationproject/services/UserService.java
@@ -0,0 +1,37 @@
+package fr.cs.authentificationproject.services;
+
+
+import fr.cs.authentificationproject.auth.AuthenticationRequest;
+import fr.cs.authentificationproject.auth.AuthenticationResponse;
+import fr.cs.authentificationproject.auth.RegisterRequest;
+import fr.cs.authentificationproject.dto.UserDto;
+import fr.cs.authentificationproject.entities.User;
+import jakarta.transaction.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+
+public interface UserService{
+
+
+  AuthenticationResponse registerAdmin(RegisterRequest request);
+
+  AuthenticationResponse registerUser(RegisterRequest request);
+
+  AuthenticationResponse authenticate(AuthenticationRequest request);
+
+  void delete(Integer id);
+
+  @Transactional
+  List<UserDto> findAll();
+
+  UserDto findById(Integer id);
+
+  List<User> getUsers();
+
+
+  Optional<User> getUserById(Integer id);
+
+  void updateUser(Integer id, String firstName, String lastName, String email, String password);
+}
